78fd11869f15f7e4a4296ee0577df94f3b0be31d
antoniom42 <antonioeslava04@iesamachado.org>
2023-01-12 19:11:35 +0100

    practicando los scripts

---
+++ JavaScript/circulos.js
@@ -0,0 +1,9 @@
+function ocultarrojo() {
+    document.getElementById('rojo').style.display='none';
+}
+function ocultarverde() {
+    document.getElementById('verde').style.display='none';
+}
+function ocultarazul() {
+    document.getElementById('azul').style.display='none';
+}
